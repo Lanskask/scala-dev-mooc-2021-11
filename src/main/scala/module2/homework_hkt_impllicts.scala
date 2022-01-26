c4725fe30e04e24085bb33de6e8c9adef1063ef5
Sergey-Murtazin <sergey-murtazin@github.com>
2022-01-26 20:14:57 +0300

    HTK Homework

--- src/main/scala/module2/homework_hkt_impllicts.scala
+++ src/main/scala/module2/homework_hkt_impllicts.scala
@@ -1,22 +1,23 @@
 package module2
 
-object homework_hkt_impllicts{
+object homework_hkt_impllicts {
 
-    /**
-      * 
-      * Доработать сигнатуру tupleF и реализовать его
-      * По итогу должны быть возможны подобные вызовы
-      *   val r1 = println(tupleF(optA, optB))
-      *   val r2 = println(tupleF(list1, list2))
-      * 
-      */
-    def tupleF[F[_], A, B](fa: F[A], fb: F[B]) = ???
+  /**
+   *
+   * Доработать сигнатуру tupleF и реализовать его
+   * По итогу должны быть возможны подобные вызовы
+   * val r1 = println(tupleF(optA, optB))
+   * val r2 = println(tupleF(list1, list2))
+   *
+   */
+  def tupleF[F[_], A, B](fa: F[A], fb: F[B]) = ???
 
 
-    trait Bindable[F[_], A] {
-        def map[B](f: A => B): F[B]
-        def flatMap[B](f: A => F[B]): F[B]
-    }
+  trait Bindable[F[_], A] {
+    def map[B](f: A => B): F[B]
+
+    def flatMap[B](f: A => F[B]): F[B]
+  }
 
 
   val optA: Option[Int] = Some(1)
